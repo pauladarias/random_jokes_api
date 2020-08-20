edd6bf49d1db47d477d6c44a8f027c61b7e378a0
pauladarias <pauladarias@gmail.com>
2020-08-20 15:24:03 +0100

    Get the setup from the joke

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const setupDiv = document.getElementById("setup");
-const punchlinDiv = document.getElementById("punchline");
+const punchlineDiv = document.getElementById("punchline");
 const punchlineBtn = document.getElementById("punchlineBtn");
-const nreJokeBtn = document.getElementById("newJokeBtn");
+const newJokeBtn = document.getElementById("newJokeBtn");
+
+// Setup an async function called getJoke
+// Create a variable called jokePromise that fetches a joke
+//from https://official-joke-api.appspot.com/jokes/programming/random
+// create a variable called joke that consumes the json data
+
+async function getJoke() {
+  const jokePromise = await fetch(
+    "https://official-joke-api.appspot.com/jokes/programming/random"
+  );
+  const joke = await jokePromise.json();
+  // Get the setup from the joke and insert it into the setupDiv element
+  joke.forEach((joke) => {
+    //const div = document.createElement("div");
+    setupDiv.innerHTML = joke.setup;
+    //document.body.appendChild(div);
+  });
+}
+
+getJoke();
