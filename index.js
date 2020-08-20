04396227990a7d84f642ae3ea4358b854e326524
pauladarias <pauladarias@gmail.com>
2020-08-20 15:09:28 +0100

    Add html, css and constant vars in js

---
+++ index.js
@@ -0,0 +1,4 @@
+const setupDiv = document.getElementById("setup");
+const punchlinDiv = document.getElementById("punchline");
+const punchlineBtn = document.getElementById("punchlineBtn");
+const nreJokeBtn = document.getElementById("newJokeBtn");
